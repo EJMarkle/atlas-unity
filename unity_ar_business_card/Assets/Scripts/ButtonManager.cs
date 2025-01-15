1f1000645315ea27a0e70b428c2c4c11be75fd8b
EJMarkle <emarkle99@gmail.com>
2025-01-14 23:02:08 -0600

    button effect, new coroutines, and finishing touches

--- unity_ar_business_card/Assets/Scripts/ButtonManager.cs
+++ unity_ar_business_card/Assets/Scripts/ButtonManager.cs
@@ -9,15 +9,27 @@ public class ButtonManager : MonoBehaviour
     [SerializeField] private GameObject instaButton;
     [SerializeField] private GameObject linkedinButton;
     [SerializeField] private GameObject gitButton;
+    [SerializeField] private GameObject hey;
 
     // Social links
     private string emailURL = "mailto:emarkle99@gmail.com";
     private string instaURL = "https://www.instagram.com/evon350/";
     private string linkedInURL = "https://www.linkedin.com/in/evanmarkledev/";
     private string gitURL = "https://github.com/EJMarkle";
+    private string heyURL = "https://www.youtube.com/watch?v=ZZ5LpwO-An4";
 
     // Reference to main camera
     private Camera mainCamera;
+
+    // Button materials
+    [SerializeField] private Material emailMaterial;
+    [SerializeField] private Material instaMaterial;
+    [SerializeField] private Material linkedinMaterial;
+    [SerializeField] private Material gitMaterial;
+
+    // Button press settings
+    private float pressDuration = 0.3f;
+    private float darknessFactor = 0.5f; 
 
     void Start()
     {
@@ -29,7 +41,7 @@ public class ButtonManager : MonoBehaviour
         // Check for touch input
         if (Input.touchCount > 0)
         {
-            Touch touch = Input.GetTouch(0); // Use GetTouch(0) instead of Input.touches[0]
+            Touch touch = Input.GetTouch(0);
 
             // Get position of touch where tap began
             if (touch.phase == TouchPhase.Began)
@@ -43,32 +55,48 @@ public class ButtonManager : MonoBehaviour
                 // Perform raycast
                 if (Physics.Raycast(ray, out hit))
                 {
-                    // Get which object was hit
                     GameObject hitObject = hit.collider.gameObject;
 
-                    // On button impact, open appropriate link
+                    // Trigger button effect and open URL of respective buttons
                     if (hitObject == emailButton)
                     {
                         Debug.Log("Email button pressed");
-                        Application.OpenURL(emailURL);
+                        StartCoroutine(ButtonPressEffect(emailMaterial, emailURL));
                     }
                     else if (hitObject == instaButton)
                     {
                         Debug.Log("Instagram button pressed");
-                        Application.OpenURL(instaURL);
+                        StartCoroutine(ButtonPressEffect(instaMaterial, instaURL));
                     }
                     else if (hitObject == linkedinButton)
                     {
                         Debug.Log("LinkedIn button pressed");
-                        Application.OpenURL(linkedInURL);
+                        StartCoroutine(ButtonPressEffect(linkedinMaterial, linkedInURL));
                     }
                     else if (hitObject == gitButton)
                     {
                         Debug.Log("GitHub button pressed");
-                        Application.OpenURL(gitURL);
+                        StartCoroutine(ButtonPressEffect(gitMaterial, gitURL));
+                    }
+                    else if (hitObject == hey)
+                    {
+                        Application.OpenURL(heyURL);
                     }
                 }
             }
         }
+    }
+
+    // button effect
+    private IEnumerator ButtonPressEffect(Material buttonMaterial, string url)
+    {
+        // get material color and darken
+        Color originalColor = buttonMaterial.color;
+        buttonMaterial.color = originalColor * darknessFactor;
+        yield return new WaitForSeconds(pressDuration);
+        buttonMaterial.color = originalColor;
+        
+        // Open URL
+        Application.OpenURL(url);
     }
 }
--- unity_ar_business_card/README.md
+++ unity_ar_business_card/README.md
@@ -1,1 +1,3 @@
 An augmented reality business card developed at Atlas School. 
+
+Developed using Vuforia and Unity, this project was a great start at using image targets to build a fluid animated experience. 
